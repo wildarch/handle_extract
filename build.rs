b6a065dc0737b82e5f668e39912dfbea7a53facd
Daan de Graaf <daandegraaf9@gmail.com>
2020-03-27 14:33:58 +0000

    Typesafety POC.

--- build.rs
+++ build.rs
@@ -1,7 +1,4 @@
 fn main() {
-    prost_build::Config::new()
-        .field_type_attribute("oak.handle.Handle", "#[handle_extract]")
-        .compile_well_known_types()
-        .compile_protos(&["src/handle.proto", "src/test_message.proto"], &["src/"])
+    prost_build::compile_protos(&["src/handle.proto", "src/test_message.proto"], &["src/"])
         .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,71 @@ pub mod oak {
     pub mod handle {
         include!(concat!(env!("OUT_DIR"), "/oak.handle.rs"));
     }
+
+    pub mod io {
+        use ::bytes::{Buf, BufMut};
+        use prost::encoding::{DecodeContext, WireType};
+
+        #[derive(Debug, Clone, PartialEq, Default)]
+        pub struct Sender<T> {
+            handle: super::handle::Sender,
+            _ty: std::marker::PhantomData<T>,
+        }
+
+        impl<T: Send + Sync + core::fmt::Debug> prost::Message for Sender<T> {
+            fn encoded_len(&self) -> usize {
+                self.handle.encoded_len()
+            }
+
+            fn clear(&mut self) {
+                self.handle.clear()
+            }
+
+            fn encode_raw<B: BufMut>(&self, buf: &mut B) {
+                self.handle.encode_raw(buf);
+            }
+
+            fn merge_field<B: Buf>(
+                &mut self,
+                tag: u32,
+                wire_type: WireType,
+                buf: &mut B,
+                ctx: DecodeContext,
+            ) -> Result<(), prost::DecodeError> {
+                self.handle.merge_field(tag, wire_type, buf, ctx)
+            }
+        }
+
+        #[derive(Debug, Clone, PartialEq, Default)]
+        pub struct Receiver<T> {
+            handle: super::handle::Receiver,
+            _ty: std::marker::PhantomData<T>,
+        }
+
+        impl<T: Send + Sync + core::fmt::Debug> prost::Message for Receiver<T> {
+            fn encoded_len(&self) -> usize {
+                self.handle.encoded_len()
+            }
+
+            fn clear(&mut self) {
+                self.handle.clear()
+            }
+
+            fn encode_raw<B: BufMut>(&self, buf: &mut B) {
+                self.handle.encode_raw(buf);
+            }
+
+            fn merge_field<B: Buf>(
+                &mut self,
+                tag: u32,
+                wire_type: WireType,
+                buf: &mut B,
+                ctx: DecodeContext,
+            ) -> Result<(), prost::DecodeError> {
+                self.handle.merge_field(tag, wire_type, buf, ctx)
+            }
+        }
+    }
 }
 
 pub mod handle_extract {
