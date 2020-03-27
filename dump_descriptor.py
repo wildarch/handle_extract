332b339b23d07a3b243c269d9764a365d6df3bac
Daan de Graaf <daandegraaf9@gmail.com>
2020-03-27 09:09:26 +0000

    Working dump.

--- dump_descriptor.py
+++ dump_descriptor.py
@@ -1,6 +1,7 @@
 from google.protobuf.descriptor_pb2 import FileDescriptorSet
 from google.protobuf import text_format
 import subprocess
+import handle_pb2
 
 DESCRIPTOR_SET_FILE_PATH = 'descriptor_set.bin'
 SRC_DIR = 'src/'
@@ -8,7 +9,8 @@ MESSAGE_PATH = SRC_DIR + 'test_message.proto'
 DUMP_FILE_PATH = "descriptor_set.textproto"
 
 protoc_ret = subprocess.run([
-    'protoc',
+    '/home/daan/Downloads/protoc/bin/protoc',
+    #'protoc',
     '--include_imports',
     '--include_source_info',
     '-o', DESCRIPTOR_SET_FILE_PATH,
@@ -25,3 +27,5 @@ fds.ParseFromString(data)
 
 with open(DUMP_FILE_PATH, "w") as f:
     f.write(text_format.MessageToString(fds))
+
+print(len(fds.file[-1].message_type[0].field[-1].options.Extensions))
